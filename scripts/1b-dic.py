8bc01dd126a2e059bd23d751db800677ae18198f
reynaldC1995 <reynald.charles3@gmail.com>
2018-11-11 13:38:49 +0100

    1b-dic.py

---
+++ scripts/1b-dic.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python36
+#########################
+#	1b-dic.py	#
+#	Prenoms		#
+#	06/11/2018	#
+#     Reynald Charles	#
+#########################
+
+listing_name = []
+
+
+
+for name in listing_name:
+  name=input("Entrer un prenom ou q pour quitter le script")
+  listing_name.append(name)
+    
+listing_name.sort()
+print(listing_name)
+
+
